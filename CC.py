3db4486ddef10f1bb48960c301e8cddb5401f59d
Luis Fabian <luisfabian@Luiss-MacBook-Air.local>
2019-08-23 22:03:17 -0400

    Updated Requirements

--- CC.py
+++ CC.py
@@ -9,7 +9,7 @@ taskQueue = []
 # Notification queue.
 # Once a pollinator is done it should send a notification to the CC.
 
-
+# Defines a Task.
 class Task:
     def __init__(self, taskType, lat, lon):
         self.type = taskType
@@ -18,35 +18,15 @@ class Task:
 
 
 # Use matplot lib for a real time graph of the drones positions.
-# while there is no notifications to take care of.
 
-# Rule Based drone control system for large drone.
+
+# Rule Based drone control system for scout drone.
 # -----------------------
-# Decision Tree.
 # define Max distance from base in x and y.
 # set and keep a steady high.
 
-# Loop thruogh the tasks and figure out closest pollinator to this task.
+# Loop through the tasks and figure out closest pollinator to this task.
 # If no task then return pollinators to base.
 # Should pollinators talk to the CC ? Should we calculate time to next task using physics.
 
 # If the control center fails (time out). Pollinators should return home.
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig, ax = plt.subplots()
-
-x = [1, 2, 3, 4]
-y = [5, 6, 7, 8]
-
-for t in range(10):
-    if t == 0:
-        points, = ax.plot(x, y, marker='o', linestyle='None')
-        ax.set_xlim(0, 10)
-        ax.set_ylim(0, 10)
-    else:
-        new_x = np.random.randint(10, size=5)
-        new_y = np.random.randint(10, size=5)
-        points.set_data(new_x, new_y)
-    plt.pause(0.5)
--- RuleBasedScoutNavigation.py
+++ RuleBasedScoutNavigation.py
@@ -1,17 +1,15 @@
+# Simulation of a Randomized Rule Base Nav System.
+
 import matplotlib.pyplot as plt
 import time
 import random
 
-# Pick a direction at random.
-# Out of bounds ?
-# Move for t seconds,
-
 # Home coordinates.
-LAT, LONG = [25.5308, 25.53095 ], [80.1834, 80.18355]
+LAT, LONG = [25.5308], [80.1834]
 
 fig, ax = plt.subplots()
 
-# Drone should no fly more than $(5) meters in any direction from home
+# Drone should no fly more than (5) meters in any direction from home
 LAT_MAX, LAT_MIN = LAT[0] + 0.00018, LAT[0] - 0.00018
 LONG_MAX, LONG_MIN = LONG[0] + 0.00018, LONG[0] - 0.00018
 
@@ -22,9 +20,9 @@ last_dir = 0
 
 while (True):
     if q == 0:
-        # Initital position.
+        # Initial position.
         points, = ax.plot(LAT, LONG, marker='o', linestyle='None')
-        ax.set_xlim(LAT_MIN, LAT_MAX)
+        ax.set_xlim(LAT_MIN , LAT_MAX)
         ax.set_ylim(LONG_MIN, LONG_MAX)
         ax.ticklabel_format(useOffset=False)
 
@@ -35,23 +33,22 @@ while (True):
         if (q != 0):
             while (abs(dir - last_dir) == 2): dir = random.randint(0, 3)
 
-        while time.time() - nav_start <= 1:
+        while time.time() - nav_start <= 0.5:
             last_dir = dir
             if dir == 0:
-                if (LONG[0] + 0.0000045) <= LONG_MAX:
-                    LONG[0] = (LONG[long_num] + 0.0000045)
-
+                if (LONG[0] + 0.0000005) <= LONG_MAX - 0.00002:
+                    LONG[0] = (LONG[long_num] + 0.000005)
             elif dir == 1:
-                if (LAT[lat_num] + 0.0000045) <= LAT_MAX:
-                    LAT[0] = (LAT[lat_num] + 0.0000045)
+                if (LAT[lat_num] + 0.0000005) <= LAT_MAX  - 0.00002:
+                    LAT[0] = (LAT[lat_num] + 0.0000005)
 
             elif (dir == 2):
-                if (LONG[0] - 0.0000045) >= LONG_MIN:
-                    LONG[0] = (LONG[long_num] - 0.0000045)
+                if (LONG[0] - 0.0000005) >= LONG_MIN + 0.00002:
+                    LONG[0] = (LONG[long_num] - 0.0000005)
 
             else:
-                if (LAT[0] - 0.0000045) >= LAT_MIN:
-                    LAT[0]= (LAT[lat_num] - 0.0000045)
+                if (LAT[0] - 0.0000005) >= LAT_MIN + 0.00002:
+                    LAT[0]= (LAT[lat_num] - 0.0000005)
 
             points.set_data(LAT, LONG)
             plt.pause(0.001)
